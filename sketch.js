1410a2c00f675a26a01d8d06625a01766f4c30ce
shrutiror <45755921+shrutiror@users.noreply.github.com>
2019-05-23 13:50:47 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ createCanvas(800, 300);
 }
 function draw() {
 background(220);
-ellipse(100 + 150, 200, 50, 50);
-ellipse(200 + 150, 200, 50, 50);
-rect(50 + 150, 160, 200, 20)
+ellipse(100 + 250, 200, 50, 50);
+ellipse(200 + 250, 200, 50, 50);
+rect(50 + 250, 160, 200, 20)
 }
