77d09b6e6b34b75ed0e40555eaef28bd551be334
shrutiror <45755921+shrutiror@users.noreply.github.com>
2019-05-24 12:21:35 +0530

    noisefunction

---
+++ noisefunction.js
@@ -0,0 +1,25 @@
+function setup() {
+	createCanvas(800,300);
+}
+
+function draw()
+ {
+	background(255,0,0);
+	//var x=width/2;
+	//var y=height/2; 
+	var diameter=50;
+	for(var i=0;i<width/diameter;i=i+1)
+	{
+	  for(var j=0;j<width/diameter;j=i+1)
+ 
+		{
+			fill(30,78,150);
+		    stroke(0,255,0);
+		    ellipse((diameter/2)+i*(diameter),
+		    	(diameter/2)+j*(diameter),
+		    	 diameter*noise(frameCount/100),
+		    	  diameter*noise(frameCount/100);
+        }
+
+	}
+}
