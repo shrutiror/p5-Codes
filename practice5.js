d52bdef1b3b00c575145094813c714093c0c20f4
shrutiror <45755921+shrutiror@users.noreply.github.com>
2019-05-24 10:26:20 +0530

    Create practice5.js

---
+++ practice5.js
@@ -0,0 +1,12 @@
+function setup() {
+createCanvas(800, 300);
+ background(1, 75, 100);
+
+}
+function draw() {
+var x = mouseX;
+var y = mouseY;
+fill(237, 34, 93);
+noStroke();
+rect(x, y, 50, 30);
+}
