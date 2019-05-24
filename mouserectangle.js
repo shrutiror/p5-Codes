535deacb9cd74e5660fb0fc4343b1e6b21156252
shrutiror <45755921+shrutiror@users.noreply.github.com>
2019-05-24 10:23:09 +0530

    Create mouserectangle.js

---
+++ mouserectangle.js
@@ -0,0 +1,11 @@
+function setup() {
+createCanvas(800, 300);
+}
+function draw() {
+  background(1, 75, 100);
+var x = mouseX;
+var y = mouseY;
+fill(237, 34, 93);
+noStroke();
+rect(x, y, 50, 30);
+}
